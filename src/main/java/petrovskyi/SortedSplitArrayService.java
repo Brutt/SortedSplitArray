6d0e93b8792f6ef3977f0405efd8484fc91ddab8
player <petrovskiy.dev@gmail.com>
2020-08-31 19:11:43 +0300

    Implemented method to find the leftmost element on the border of splitting: - added findEdge() recursive method and test to it

--- src/main/java/petrovskyi/SortedSplitArrayService.java
+++ src/main/java/petrovskyi/SortedSplitArrayService.java
@@ -4,4 +4,23 @@ public class SortedSplitArrayService {
     public int findElem(int[] arr, int value) {
         return 0;
     }
+
+    int findEdge(int arr[], int startIndex, int finishIndex) {
+        if (startIndex == finishIndex) {
+            return startIndex;
+        }
+
+        int middle = (startIndex + finishIndex) / 2;
+        if (middle < finishIndex && arr[middle] > arr[middle + 1]) {
+            return middle;
+        }
+        if (middle > startIndex && arr[middle] < arr[middle - 1]) {
+            return (middle - 1);
+        }
+        if (arr[startIndex] >= arr[middle]) {
+            return findEdge(arr, startIndex, middle - 1);
+        }
+
+        return findEdge(arr, middle + 1, finishIndex);
+    }
 }
--- src/test/java/petrovskyi/SortedSplitArrayTest.java
+++ src/test/java/petrovskyi/SortedSplitArrayTest.java
@@ -18,4 +18,18 @@ public class SortedSplitArrayTest {
 
         assertEquals(4, index);
     }
+
+    @Test
+    @DisplayName("Should find a leftmost element on the border of split array parts and return index")
+    public void searchLeftmostBorderElementAndReturnIndex() {
+        int[] arr = new int[]{5, 6, 7, 1, 2, 3, 4};
+
+        SortedSplitArrayService service = new SortedSplitArrayService();
+
+        int startIndex = 0;
+        int finishIndex = arr.length-1;
+        int index = service.findEdge(arr, startIndex, finishIndex);
+
+        assertEquals(2, index); //number 7 has 1st index
+    }
 }
